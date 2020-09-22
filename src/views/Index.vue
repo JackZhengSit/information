8327f2c0e16db13f6f6cce015915cce888fae16d
Jack Zheng <JackZhengSit@outlook.com>
2020-09-22 17:11:40 +0800

    fist

---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container>
+    <el-header>
+      <router-view name="header"></router-view>
+    </el-header>
+    <el-main>
+      <router-view name="main"></router-view>
+    </el-main>
+    <el-footer>
+      <router-view name="footer"></router-view>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
